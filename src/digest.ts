ba0d9cd93223353226124ad18b73621fc2aed1b6
Hidetake Iwata <int128@gmail.com>
2021-09-14 14:36:28 +0900

    Use different cache between GitHub hosted runner and self hosted runner (#15)

--- src/digest.ts
+++ src/digest.ts
@@ -1,5 +1,7 @@
 import { createHash } from 'crypto'
 import * as fs from 'fs'
+import * as os from 'os'
+import * as path from 'path'
 
 export const computeDigest = async (name: string): Promise<string> => {
   const h = createHash('sha256')
@@ -13,4 +15,12 @@ export const computeDigest = async (name: string): Promise<string> => {
   })
   p.finally(() => f.close())
   return p
+}
+
+export const computeCacheKeyForRelativePath = (): string => {
+  const workspace = process.env['GITHUB_WORKSPACE'] ?? process.cwd()
+  const rp = path.relative(workspace, os.homedir())
+  const h = createHash('sha256')
+  h.write(rp)
+  return h.digest('hex')
 }
